a34f2beabdc7dd2029b74fc1c0d1d320d0b0fdd1
Roberto <robpalacios1@hotmail.com>
2018-12-02 18:39:35 -0500

    Agregando Validaciones

--- app/models/room.rb
+++ app/models/room.rb
@@ -14,7 +14,7 @@
 
 class Room < ActiveRecord::Base
 	validates :title, presence: true
-	validates :description, length: { maximus: 400 }
+	validates :description, length: { maximum: 400 }
 	validates :beds, numericality: { only_integer: true}
 	validates :guests, numericality: { only_integer: true} 
 	validates :image_url, presence: true
