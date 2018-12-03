6fcc83e19b5ca98fc286c6c14d71c0d247967b1c
Roberto <robpalacios1@hotmail.com>
2018-12-03 13:58:56 -0500

    Agregando nuevo campo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150510183516) do
+ActiveRecord::Schema.define(version: 20181203150123) do
 
   create_table "rooms", force: :cascade do |t|
     t.string   "title"
