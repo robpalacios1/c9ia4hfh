ad63e0f8714399d81156bb7bd84b5fde4f9658c6
Roberto <robpalacios1@hotmail.com>
2018-12-05 23:44:15 -0500

    Creando ruta edicion

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -7,6 +7,10 @@ class RoomsController < ApplicationController
     @room = Room.new
   end
 
+   def edit
+    @room = Room.find(params[:id])
+  end
+
   def create
     @room = Room.new(room_params)
     if @room.save
@@ -20,8 +24,4 @@ class RoomsController < ApplicationController
     def room_params
       params.require(:room).permit(:title, :description, :beds, :guests, :image_url, :price_per_night)
     end
-
-  def edit
-    @room = Room.find(params[:id])
-  end
 end
